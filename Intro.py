07daf30c20c9001c235bff75caacf242cdeadd10
databricks-test <databricks-test>
2023-10-31 06:48:30 +0000

    Added a notebook

---
+++ Intro.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC #This is to test the databricks integration with GitHub
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC <div><center>
+# MAGIC <h2 style="text-alignment:center">Code Spectra</h2>
+# MAGIC </center></div>
